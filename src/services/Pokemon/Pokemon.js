81229dab260443d376498618fa186638884d3958
KAYA jordan <kayajordan21@gmail.com>
2022-08-01 19:34:26 +0200

    modification du hook dans le but d'afficher toutes les infos sur chaque pokemon

--- src/services/Pokemon/Pokemon.js
+++ src/services/Pokemon/Pokemon.js
@@ -6,22 +6,38 @@ function usePokemons() {
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null)
     const [pokemons, setPokemons] = useState([]);
+    const [url, setUrl] = useState('https://pokeapi.co/api/v2/pokemon/');
+    const [nextUrl, setNextUrl] = useState(null);
+    const [previewUrl, setPreviewUrl] = useState(null);
+    
 
+    const fetchData=async()=>{
+        setLoading(true);
+        try {
+            const response = await axios.get(url);
+            setNextUrl(response.data.next)
+            setPreviewUrl(response.data.previous)
+            getPokemon(response.data.results)
+            setLoading(false);
+        } catch (error) {
+            setError(error);
+        }
+    }
+    const getPokemon = async(response)=>{
+       response.map(async(item)=>{
+        const resultat=await axios.get(item.url);
+        setPokemons(state=>{
+           state=[...state,resultat.data]
+           state.sort((a,b)=>a.id>b.id?1:-1)
+           return state;
+        })
+       })
+    }
     useEffect(() => {
         /***fetchData function */
-        async function fetchData() {
-            setLoading(true);
-            try {
-                const { data } = await axios.get('https://pokeapi.co/api/v2/pokemon/');
-                setPokemons(data.results);
-                setLoading(false);
-            } catch (error) {
-                setError(error);
-            }
-        }
         fetchData();
-    }, [])
-    return {loading,error,pokemons}
+    }, [url])
+    return { loading, error, pokemons }
 }
 
 export default usePokemons
