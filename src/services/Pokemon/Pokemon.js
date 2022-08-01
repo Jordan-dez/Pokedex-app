1289ee6578ce5a683f8f69d37a4fa8541a054dff
KAYA jordan <kayajordan21@gmail.com>
2022-08-01 14:21:38 +0200

    ajout du block try catch

--- src/services/Pokemon/Pokemon.js
+++ src/services/Pokemon/Pokemon.js
@@ -2,19 +2,26 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function usePokemons() {
+    /**déclaration et initialization des hooks */
     const [loading, setLoading] = useState(false);
-    const [error,setError]=useState(null)
+    const [error, setError] = useState(null)
     const [pokemons, setPokemons] = useState([]);
+
     useEffect(() => {
+        /***fetchData function */
         async function fetchData() {
             setLoading(true);
-            const { data } = await axios.get('https://pokeapi.co/api/v2/pokemon/');
-            setPokemons(data.results);
-            setLoading(false);
+            try {
+                const { data } = await axios.get('https://pokeapi.co/api/v2/pokemon/');
+                setPokemons(data.results);
+                setLoading(false);
+            } catch (error) {
+                setError(error);
+            }
         }
         fetchData();
     }, [])
-    return {}
+    return {loading,error,pokemons}
 }
 
 export default usePokemons
