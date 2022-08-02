87c616414c72763a3a224a5fd9ea7ec0284bb70b
KAYA jordan <kayajordan21@gmail.com>
2022-08-02 22:38:26 +0200

    adding service with fetch

--- src/services/Pokemon/Pokemon.js
+++ src/services/Pokemon/Pokemon.js
@@ -9,9 +9,20 @@
 // export default usePokemons
 
 
-import axios from 'axios';
+// import axios from 'axios';
 
-export default async function getPokemons(url) {
-    const { data } = await axios.get(url);
-    return data;
-}
+// export default async function getPokemons(url) {
+//     const { data } = await axios.get(url);
+//     return data;
+// }
+const URL = 'https://pokeapi.co/api/v2/pokemon';
+
+export const getPokemons = () => {
+  return fetch(URL).then((response) => response.json());
+};
+
+export const getMorePokemons = (offset) => {
+  return fetch(`${URL}?offset=${offset}&limit=20`).then((response) =>
+    response.json()
+  );
+};
