6e47c9e13986f33a05d080950e0508db2ff0e582
KAYA jordan <kayajordan21@gmail.com>
2022-08-04 19:30:55 +0200

    ajout du store

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,8 @@
 import { combineReducers, legacy_createStore } from "redux";
+import LocalStorage from "../reducers/LocalStorageReducers.js"
 
 const rootReducers=combineReducers({
-
+    pokedex: LocalStorage
 })
 
 const store= legacy_createStore(rootReducers,{},window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,71 +1,38 @@
+/******
+ * Tous les imports sont ici
+ */
 import React, { useEffect, useState } from 'react'
 import ListPokemon from '../../components/ListPokemon/ListPokemon';
 import PokemonCard from '../../components/PokemonCard/PokemonCard';
-// import usePokemons from '../../services/Pokemon/Pokemon/Pokemon'
 import axios from 'axios';
-import { getPokemons, getMorePokemons } from '../../services/Pokemon/Pokemon/';
+import { getPokemons, getMorePokemons } from '../../services/Pokemon/Pokemons/';
+import SearchBar from '../../components/SearchBar/SearchBar'
+
 
 const Home = () => {
-  // const [pokeData , setPokeData] = useState([]);
-  // const [loading, setLoading] = useState(true);
-  // const [url, setUrl] = useState("https://pokeapi.co/api/v2/pokemon/");
-  // const [nextUrl, setNextUrl] = useState();
-  // const [prevUrl, setPrevUrl] = useState();
-  // const [disable, setDisable] = React.useState(true);
 
-
-  // const pokeFunc = async() => {
-
-
-  //     const res = await axios.get(url);
-
-  //     setNextUrl(res.data.next);
-  //     setPrevUrl(res.data.previous);
-  //     getPokemonData(res.data.results);
-  //     setLoading(false)
-
-  //     console.log("PREV", res.data.previous);
-  //     console.log("NEXT", res.data.next);
-
-  //     if (res.data.previous != null) {
-  //         setDisable(false);
-  //     } else {
-  //         setDisable(true);
-  //     }
-  // }
-
-  // const getPokemonData = async(res) => {
-  //     res.map(async(item)=>{
-  //         const result =  await axios.get(item.url);
-  //         // console.log("MAP DATA", result.data);
-  //         setPokeData(state => {
-  //             state = [...state,result.data]
-  //             state.sort((a,b) => a.id>b.id?1:-1)
-  //             return state;
-  //         })
-  //     })
-  // }
-
-  // console.log("pokedat",pokeData)
-  // useEffect(() => {
-  //     pokeFunc()
-  // }, [url])
-
+  const [loading, setLoading] = useState(false);
   const [pokemons, setPokemons] = useState([]);
   const [count, setCount] = useState(1);
+  const [searchValue,setSearchValue] = useState(null);
+
 
   useEffect(() => {
+    setLoading(true);
     getPokemons().then((data) => {
       setPokemons(data);
+      setLoading(false)
     });
   }, []);
 
   const morePokemon = () => {
+    setLoading(true);
     getMorePokemons(count + 19).then((data) => {
       setPokemons(data);
       setCount((prevCount) => {
         return prevCount + 20;
       });
+      setLoading(false)
     });
   };
 
@@ -77,28 +44,22 @@ const Home = () => {
       });
     });
   };
-{/* <div>
-      {
-        loading ? <p>loading</p> : <ListPokemon pokemons={pokeData} />
-      }
-    </div> */}
   return (
     <>
+      <SearchBar/>
     <div className='cards'>
-    {pokemons.results &&
-      pokemons.results.map((pokemon, index) => {
-        return <PokemonCard key={index} {...pokemon} id={count + index} />;
-      })}
+    { loading?<p>loading</p> : <ListPokemon pokemons={pokemons.results} count={count}/>
+    }
   </div>
   <div className='footer'>
     {
       count > 1?<div className='btn' onClick={lessPokemon}>
-      <h3 className='btn__text'>Back</h3>
+      <h3 className='btn__text'>Precedent</h3>
     </div>:null
     }
     
     <div className='btn' onClick={morePokemon}>
-      <h3 className='btn__text'>Next</h3>
+      <h3 className='btn__text'>Suivant</h3>
     </div>
   </div>
   </>
